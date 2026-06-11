e4a00d8f4af763548645bd3979fcab3c68ed4150
GabrielliYasmin <gabrielliyasmin28@gmail.com>
2026-06-11 14:33:45 -0300

    Add click event to show message and hide button

---
+++ script.js
@@ -0,0 +1,9 @@
+document
+  .getElementById("botao")
+  .addEventListener("click", () => {
+
+    document.getElementById("mensagem").style.display = "block";
+
+    document.getElementById("botao").style.display = "none";
+
+});
